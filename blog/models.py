a7f1940eefd286838e82fe85bcbf3a00d7cf541d
jieun8 <wwooww25@gmail.com>
2016-07-12 14:15:39 +0900

    startproject, startapp blog

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+import re
+from django.db import models
+from django.forms import ValidationError
+from django.utils import timezone
+
+def lnglat_validator(value):
+    if not re.match(r'^(\d+\.?\d*),(\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='제목') #한줄입력
+    content = models.TextField(help_text='Markdown문법을 써주세요.') #여러줄 입력
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, validators=[lnglat_validator], help_text='경도, 위도 포맷으로 입력')
+    created_at = models.DateTimeField(default=timezone.now)
+    text_field = models.IntegerField(default=10)
+
+    def __str__(self):
+        return self.title
