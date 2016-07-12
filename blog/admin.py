d7adb30030418b25b93c5d28e1c90141233e5277
jieun8 <wwooww25@gmail.com>
2016-07-12 16:06:34 +0900

    add PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content']
+
+
+admin.site.register(Post, PostAdmin)
