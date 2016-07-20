d435db79816b05c36a591b913d88e306afaed989
jieun8 <wwooww25@gmail.com>
2016-07-20 15:00:11 +0900

    change model

--- pockemon/admin.py
+++ pockemon/admin.py
@@ -1,16 +1,33 @@
 from django.contrib import admin
-from pockemon.models import User, Pockemon
+from pockemon.models import User, Pockemon, Region, Capture
 from .forms import PockemonForm
+
+
+class CaptureInline(admin.TabularInline):
+    model = Capture
+    extra = 2
 
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'user_name', 'user_level']
+    inlines = [CaptureInline]
 
 
 class PockemonAdmin(admin.ModelAdmin):
-    list_display = ['id', 'po_name', 'po_level', 'catched_by', 'lnglat', 'catched_when']
+    list_display = ['id', 'po_name', 'po_level']
     form = PockemonForm
+    inlines = [CaptureInline]
 
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    inlines = [CaptureInline]
+
+
+#class CaptureAdmin(admin.ModelAdmin):
+#    list_display = ['id', 'user', 'pockemon', 'region']
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Pockemon, PockemonAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(Capture)
--- pockemon/models.py
+++ pockemon/models.py
@@ -7,20 +7,38 @@ class User(models.Model):
 
     def __str__(self):
         return self.user_name
+        #return "{}/{}/{}".format(self.id, self.user_name, self.user_level)
 
 
 class Pockemon(models.Model):
-    catched_by = models.ForeignKey(User)
-    catched_when = models.DateTimeField(default=timezone.now)
-    lnglat = models.CharField(max_length=50, blank=True)
+    #catched_by = models.ForeignKey(User)
+    #catched_when = models.DateTimeField(default=timezone.now)
+    #lnglat = models.CharField(max_length=50, blank=True)
     po_name = models.CharField(max_length=10)
     po_level = models.IntegerField(default=1)
 
     def __str__(self):
         return self.po_name
 
-    def lat(self):
-        return self.catched_where.split(',')[1]
+    #def lat(self):
+    #    return self.catched_where.split(',')[1]
 
-    def lng(self):
-        return self.catched_where.split(',')[0]
+    #def lng(self):
+    #    return self.catched_where.split(',')[0]
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Capture(models.Model):
+    user = models.ForeignKey(User)
+    pockemon = models.ForeignKey(Pockemon)
+    region = models.ForeignKey(Region)
+    when = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "{}가 {}에서 {}에 {}를 포획!".format(self.user, self.region, self.when, self.pockemon)
