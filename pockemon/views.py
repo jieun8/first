12e122efd2629f902697e8bfd332a91c67778045
jieun8 <wwooww25@gmail.com>
2016-07-21 16:48:10 +0900

    pockemon_list

--- pockemon/views.py
+++ pockemon/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from pockemon.models import Pockemon
 
-# Create your views here.
+def pockemon_list(request):
+    qs = Pockemon.objects.all()
+    return render(request, "pockemon_list.html",
+        {'pockemon_list': qs})
